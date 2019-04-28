d8b29bb5e9a87930896cce444a2ed2697c91d18b
yuanyuanheng <yuanyuanheng@126.com>
2019-04-28 08:58:45 +0800

    初始项目
    1、ctrl + M 放大缩小
    2、alt + Q 关闭
    3、ctrl + S 保存
    4、ctrl + 箭头向下或者向上查询

---
+++ note/note/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace note
+{
+    partial class Note
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Note));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItem_Clear = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem_save = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem_check = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(481, 364);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_Clear,
+            this.ToolStripMenuItem_save,
+            this.ToolStripMenuItem_check});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 70);
+            // 
+            // ToolStripMenuItem_Clear
+            // 
+            this.ToolStripMenuItem_Clear.Name = "ToolStripMenuItem_Clear";
+            this.ToolStripMenuItem_Clear.Size = new System.Drawing.Size(100, 22);
+            this.ToolStripMenuItem_Clear.Text = "清空";
+            this.ToolStripMenuItem_Clear.Click += new System.EventHandler(this.ToolStripMenuItem_Clear_Click);
+            // 
+            // ToolStripMenuItem_save
+            // 
+            this.ToolStripMenuItem_save.Name = "ToolStripMenuItem_save";
+            this.ToolStripMenuItem_save.Size = new System.Drawing.Size(100, 22);
+            this.ToolStripMenuItem_save.Text = "保存";
+            this.ToolStripMenuItem_save.Click += new System.EventHandler(this.ToolStripMenuItem_save_Click);
+            // 
+            // ToolStripMenuItem_check
+            // 
+            this.ToolStripMenuItem_check.Name = "ToolStripMenuItem_check";
+            this.ToolStripMenuItem_check.Size = new System.Drawing.Size(100, 22);
+            this.ToolStripMenuItem_check.Text = "查询";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 34);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.Location = new System.Drawing.Point(0, 375);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(481, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            // 
+            // Note
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(481, 398);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Note";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Clear;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_save;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_check;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ note/note/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace note
+{
+    
+
+    public partial class Note : Form
+    {
+        public Note()
+        {
+            InitializeComponent();
+            this.Size = new Size(480, 400);
+            int nWidth = SystemInformation.WorkingArea.Width - this.Width;
+            int nHeight = SystemInformation.WorkingArea.Height - this.Height;
+            this.Location = new Point(nWidth, nHeight);
+
+           
+
+        }
+
+        private void ToolStripMenuItem_Clear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+
+        private void ToolStripMenuItem_save_Click(object sender, EventArgs e)
+        {
+            save();  
+        }
+
+      
+
+        /// <summary>
+        /// 保存界面的值到csv文件和变量,相同不能保存。
+        /// </summary>
+        private void save()
+        {
+            string strTitle = this.textBox1.Text.Replace(',', '&');
+            if (!string.IsNullOrEmpty(strTitle))
+            {
+                string strTemp = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + ",";
+
+                string strContext = this.richTextBox1.Text.Replace(',', '&');
+                
+                strContext = strContext.TrimEnd().Replace('\n', ',');
+
+                strTemp += strTitle + ",";
+                strTemp += strContext + '\n';
+                try
+                {
+                    if (m_strTitle == strTitle && m_strContent == strContext)
+                    {
+                        return;
+                    }
+                    
+                    if (File.Exists(m_strPath))
+                    {
+                        string[] strList = File.ReadAllLines(m_strPath);
+                        for (int n = 0; n < strList.Length; n++)
+                        {
+                            if (strList[n].Contains(strTitle) && strList[n].Contains(strContext))
+                            {
+                                return;
+                            }
+
+                        }
+                    
+                    }
+
+                    m_strTitle = strTitle ;
+                    m_strContent = strContext;
+
+                    File.AppendAllText(m_strPath, strTemp);
+                }
+                catch
+                {
+                    return;
+                }
+
+
+            }
+        }
+
+        private void clear() 
+        {
+            m_strContent = string.Empty;
+            m_strTitle = string.Empty;
+
+            this.textBox2.Clear();
+            this.richTextBox1.Clear();
+            this.textBox1.Clear();
+        }
+
+        /// <summary>
+        /// richTextBox1键盘响应
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode == Keys.Q)
+            {
+                e.Handled = true;
+                this.Close();
+            }
+            else if(e.Control && e.KeyCode == Keys.S)
+            {
+                e.Handled = true;
+                save();
+            }
+            else if (e.Control && e.KeyCode == Keys.B)
+            {
+                e.Handled = true;
+                clear();
+            }
+            else if (e.Control && e.KeyCode == Keys.M)
+            {
+                e.Handled = true;
+                controlForm();
+            }
+
+            else if (e.Control && e.KeyValue == 38)
+            {
+                e.Handled = true;
+                readUp();
+            }
+
+            else if (e.Control && e.KeyValue == 40)
+            {
+                e.Handled = true;
+                readDown();
+            }
+            
+        }
+
+        /// <summary>
+        /// 向上读取，标题空，读取最后一行。标题不为空，查找并向上读取。
+        /// </summary>
+        private void readUp()
+        {
+            string[] strList = File.ReadAllLines(m_strPath);
+            if (string.IsNullOrEmpty(m_strTitle) && strList.Length > 0)
+            {
+                set(strList[strList.Length - 1]);
+                return;
+            }
+
+            for (int n = 0; n < strList.Length; n++)
+            {
+                if (strList[n].Contains(m_strTitle) && strList[n].Contains(m_strContent) && n - 1 >= 0)
+                {
+                    set(strList[n - 1]);
+                    return;
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// 向下读取，标题空，读取最前一行。标题不为空，查找并向下读取。
+        /// </summary>
+        private void readDown()
+        {
+            string[] strList = File.ReadAllLines(m_strPath);
+            if (string.IsNullOrEmpty(m_strTitle) && strList.Length > 0)
+            {
+                set(strList[0]);
+                return;
+            }
+
+            for (int n = 0; n < strList.Length; n++)
+            {
+                if (strList[n].Contains(m_strTitle) && strList[n].Contains(m_strContent) && n + 1 < strList.Length)
+                {
+                    set(strList[n + 1]);
+                    return;
+                }
+
+            }
+
+        }
+        /// <summary>
+        /// 从csv里读出值，设置到界面和变量。
+        /// </summary>
+        /// <param name="strData"></param>
+        private void set(string strData) 
+        {
+            clear();
+
+            string[] strList = strData.Split(',');
+            if (strList.Length > 1)
+            {
+                this.m_strTitle = strList[1] ;
+                this.textBox1.Text = strList[1];
+                this.textBox2.Text = strList[0];
+
+            }
+
+           
+            for (int n = 2; n < strList.Length; n++ )
+            {
+                m_strContent += strList[n];
+                if (strList.Length != n+1) 
+                {
+                    m_strContent += ",";
+                }
+
+                this.richTextBox1.Text += strList[n].Replace('&', ',') + '\n';  
+            }
+            
+        }
+
+        
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            controlForm();
+        }
+
+        /// <summary>
+        /// 缩小放大窗口
+        /// </summary>
+        private void controlForm() 
+        {
+            if (this.Size.Height == 400)
+            {
+                this.Size = new Size(50, 50);
+            }
+            else
+            {
+                this.Size = new Size(480, 400);
+            }
+
+            int nWidth = SystemInformation.WorkingArea.Width - this.Width;
+            int nHeight = SystemInformation.WorkingArea.Height - this.Height;
+            this.Location = new Point(nWidth, nHeight);
+        }
+
+        string m_strPath = "E://Note.csv";
+        string m_strContent;
+        string m_strTitle;
+
+       
+
+        
+    }
+}
